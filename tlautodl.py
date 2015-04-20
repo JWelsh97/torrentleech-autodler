f0c8ba6cd5e7a6446a433b3de65c4673ebc21d66
Jack Welsh <jackwelsh9717@hotmail.com>
2015-04-20 16:57:59 +0100

    Update tlautodl.py
    Put most of the bot into functions to tidy the code up, added downloading of the torrent, also added a config where you will enter your info.

--- tlautodl.py
+++ tlautodl.py
@@ -2,39 +2,57 @@ __author__ = 'legacy'
 import socket
 import string
 import re
-# Enter nickname and realname, so the server can identify you
-nickname = "GG"
-realname = "GG"
+import requests
+import cfg
 
-host = "irc.torrentleech.org"
-port = 7011
+URL = "http://torrentleech.org/rss/download/"
 
-readbuffer = ""
+def connect(irc_address, port, nickname, realname):
+    global socket
+    socket = socket.socket()
+    socket.connect((irc_address, port))
+    socket.send("NICK %s\r\n" % nickname)
+    socket.send("USER %s 0 * : %s\r\n" % (nickname, realname))
+    return socket
 
-socket = socket.socket()
-socket.connect((host, port))
-socket.send("NICK %s\r\n" % nickname)
-socket.send("USER %s 0 * : %s\r\n" % (nickname, realname))
-while 1:
-    readbuffer = readbuffer + socket.recv(1024)
+socket = connect(cfg.irc_address, cfg.port, cfg.nickname, cfg.realname)
+
+
+def reading_lines():
+    readbuffer = ""
+    readbuffer = readbuffer + socket.recv(2048)
     temp = string.split(readbuffer, "\n")
-    readbuffer = temp.pop( )
+    readbuffer = temp.pop()
+    return temp
+
+
+
+
+def download_torrent(regex):
+        session = requests.Session()
+        response = session.get("%s%s/%s/%s.torrent" % (URL, regex.torrentid, cfg.rsskey, regex.name), headers=cfg.headers, stream=True)
+        with open("%s%s.torrent" % (cfg.directory, name), 'wb') as torrent_file:
+            torrent_file.write(response.content)
+
+while 1:
+    temp = reading_lines()
 
     for line in temp:
-        print line
-        # Check if this is an announce, test will be a value if it is
-        # Regex parser
-        test = re.search("<(.*)\s::\s(.*)>\s*Name:'(.*)'\suploaded\sby\s'*(.*)'.*(http.*)", line)
-        if test:
-            category = test.group(1)
-            subcategory = test.group(2)
-            name = test.group(3)
-            uploader = test.group(4)
-            link = test.group(5)
+        print line.replace("\r", "")
+        regex = re.search("<(.*)\s::\s(.*)>\s*Name:'(.*)'\suploaded\sby\s'*(.*)'.*(http.*).*torrent/(.*)", line)
 
+        if regex:
+            category = regex.group(1)
+            subcategory = regex.group(2)
+            name = regex.group(3)
+            uploader = regex.group(4)
+            link = regex.group(5)
+            torrentid = regex.group(6).replace("\r", "")
+            download_torrent(regex)
+
+        # This will reply back to the server if it sends out a PING)
         line = line.split(" ")
-        # This will reply back to the server if it sends out a PING
-        if line[0] == "PING" :
+        if line[0] == "PING":
             socket.send("PONG %s\r\n" % line[1])
         if line[1] == "001":
-            socket.send("JOIN #tlannounces\r\n")
+            socket.send("JOIN %s\r\n" % cfg.announce_channel)
