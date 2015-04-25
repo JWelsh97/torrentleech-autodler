97f12282bcf3122b871c8c7362a1d905001520e5
Xeronel <ripster1121@gmail.com>
2015-04-25 17:14:01 -0400

    Removed unneeded code Renamed variables to make more sense PEP8 cleanup

--- module.py
+++ module.py
@@ -2,6 +2,12 @@ __author__ = 'jack'
 
 from devautodler import Config, IRC
 
+# Read config
 config = Config("config.yaml")
-irc_class = IRC(config.irc_address, config.irc_port, config.irc_nickname, config.irc_realname, config.irc_announce_channel)
-client = irc_class.client()
+
+# Connect to IRC
+irc = IRC(config.irc_address,
+          config.irc_port,
+          config.irc_nickname,
+          config.irc_realname,
+          config.irc_announce_channel)
